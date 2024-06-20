3690e6b0b2b4593bffdd10fd4555a1db733d158e
LazurcaDaniel <daniel.lazurca@student.uaic.ro>
2024-06-20 15:39:42 +0300

    added svg-s to childPhotos for remove and add to favorite

--- controller/childPhotosController.php
+++ controller/childPhotosController.php
@@ -103,7 +103,7 @@ if ($children != "") {
                 $stmt = $conn->prepare("SELECT * FROM images WHERE child_ID in ($in)");
                 $stmt->execute($childrenIds);
                 echo '<div class="">';
-
+                $endl = "\n";
                 while ($result = $stmt->fetch()) {
                     $mediaFilePath = pathinfo($result['Picture'], PATHINFO_FILENAME);
 
@@ -115,7 +115,28 @@ if ($children != "") {
                         echo $result['uploadDate'];
                         echo '</span>';
                     } else {
-                        echo '<span class="image-preview">' . $title;
+                        echo '<span class="image-preview">';
+                        echo '  <div class="nameContainer">' . $endl;
+                        echo '      <svg onclick="location.href=\'deleteChild.php?ID=' . $child->getID() . '\'" aria-hidden="true" focusable="false" data-prefix="fad"' . $endl;
+                        echo '          data-icon="square-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="-200 -200 700 700"' . $endl;
+                        echo '          class="svg-inline--fa fa-square-xmark fa-w-14 fa-xs" width="3rem" height="3rem">' . $endl;
+                        echo '          <g class="fa-group">' . $endl;
+                        echo '              <path fill="currentColor"' . $endl;
+                        echo '                  d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"' . $endl;
+                        echo '                  class="fa-secondary"></path>' . $endl;
+                        echo '          </g>' . $endl;
+                        echo '      </svg>' . $endl;
+                        echo '      <svg onclick="location.href=\'editChild.php?ID=' . $child->getID() . '\'" aria-hidden="true" focusable="false" data-prefix="fad"' . $endl;
+                        echo '          data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="-30 -230 800 800"' . $endl;
+                        echo '          class="svg-inline--fa-regular fa-star fa-w-14 fa-xs" width="3rem" height="3rem">' . $endl;
+                        echo '          <g class="fa-group">' . $endl;
+                        echo '              <path fill="currentColor"' . $endl;
+                        echo '                  d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"
+                                                class="fa-primary"></path>' . $endl;
+                        echo '          </g>' . $endl;
+                        echo '      </svg>' . $endl;
+                        echo '  </div>' . $endl;
+                        echo $title;
                         echo '<img class="image-preview-img" src="' . $result['Picture'] . '" alt="' . $title . '" onclick="loadHighResImage(this.src, this.alt)"><br>';
                         echo $result['uploadDate'];
                         echo '</span>';
